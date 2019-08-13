351c8529654c189fdfdb4d0cf9e278bbfca35f49
JoyTomi <492774245@qq.com>
2019-08-13 10:03:45 +0800

    加入 touchscale 和渐变
    1、yarn add react-native-linear-gradient
    
    2、react-native link react-native-linear-gradient
    
    3、需要cd ios && pod install

--- ReactNativeLearn/Views/Activity.js
+++ ReactNativeLearn/Views/Activity.js
@@ -3,20 +3,60 @@ import { View, Text } from "react-native";
 import { createStackNavigator } from "react-navigation";
 import RNWebView from "./FuncListView/RNWebView"
 import { Button } from "react-native-elements";
-
+import { ListItem } from 'react-native-elements'
+import TouchableScale from "react-native-touchable-scale"; 
+import LinearGradient from "react-native-linear-gradient"; 
 
 class FuncListView extends React.Component {
   static navigationOptions = {
     title: '活动'
   };
   render() {
+    const list = [
+      {
+        title: 'webView',
+        subtitle: 'av-timer',
+        head_url:"http://b-ssl.duitang.com/uploads/item/201811/04/20181104074412_wcelx.jpg"
+      },
+      {
+        title: 'react-native-element',
+        subtitle: 'flight-takeoff',
+        head_url:"http://b-ssl.duitang.com/uploads/item/201811/04/20181104074412_wcelx.jpg"
+      },
+    ]
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>First Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate("webView")}
-        />
+      <View>
+          {
+            list.map((item, i) => (
+              <ListItem
+              Component={TouchableScale}
+              friction={90} //
+              tension={100} // These props are passed to the parent component (here TouchableScale)
+              activeScale={0.95} //
+              linearGradientProps={{
+                colors: ['#FF9800', '#F44336'],
+                start: 1,
+                end: 0.2,
+              }}
+              ViewComponent={LinearGradient} // Only if no expo
+              // title="Chris Jackson"
+              // titleStyle={{ color: 'white', fontWeight: 'bold' }}
+              // subtitleStyle={{ color: 'white' }}
+              // subtitle="Vice Chairman"
+              // chevronColor="white"
+              // chevron
+
+
+
+                key={i}
+                title={item.title}
+                subtitle={item.subtitle}
+                leftAvatar={{ source: { uri: item.head_url} }}
+                onPress={() => this.props.navigation.push('webView')}
+              />
+            ))
+          }
       </View>
     );
   }
